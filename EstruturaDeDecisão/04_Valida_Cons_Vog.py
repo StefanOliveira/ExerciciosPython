9bb62f5a6a94d82c66a12292256e0b694d97dd68
Stefan de Oliveira <carlos.stefan86@yahoo.com.br>
2018-06-22 12:08:38 -0300

    EstruturaDeDecisão

--- EstruturaDeDecisão/04_Valida_Cons_Vog.py
+++ EstruturaDeDecisão/04_Valida_Cons_Vog.py
@@ -1,4 +1,8 @@
 #Valida Letra - Consoante ou Vogal
 
-aValLtr = input('Digite uma letra para validação: ').upper
+aValLtr = input('Informe uma letra: ').upper()
 
+if ('AEIOU'.find(aValLtr) >= 0):
+    print ('A letra',aValLtr,'é uma vogal!')
+else:
+    print ('A letra',aValLtr,'é uma consoante!')
