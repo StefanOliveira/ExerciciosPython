6655238cc13ee0d50b7d1cea6a316dac9eeff6e3
Stefan de Oliveira <carlos.stefan86@yahoo.com.br>
2018-06-25 16:04:24 -0300

    EstruturaDeDados

--- EstruturaDeDecisão/06_Menor_Num.py
+++ EstruturaDeDecisão/06_Menor_Num.py
@@ -3,7 +3,7 @@ nNum1 = int(input('Digite o primeiro número: '))
 nNum2 = int(input('Digite o primeiro número: '))
 nNum3 = int(input('Digite o primeiro número: '))
 
-#Validando o maior número
+#Validando o menor número
 if(nNum1 == nNum2) and (nNum1 == nNum3):
     print('Os números são iguais!')
 elif(nNum1 < nNum2) and (nNum1 < nNum3):
@@ -12,5 +12,15 @@ elif(nNum2 < nNum3):
     print('O menor número é o:',nNum2)
 else:
     print('O menor número é o:',nNum3)
+
+#Validando o maior número
+if(nNum1 == nNum2) and (nNum1 == nNum3):
+    print('Os números são iguais!')
+elif(nNum1 > nNum2) and (nNum1 > nNum3):
+    print('e o maior número é o:',nNum1)
+elif(nNum2 > nNum3):
+    print('e o maior número é o:',nNum2)
+else:
+    print('e o maior número é o:',nNum3)
 
 
